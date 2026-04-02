109ea41f1bb1153455477eb595ec6407f3b1d5df
kodio.cz <info@kodio.cz>
2026-04-02 02:05:26 +0200

    Contanct info address fix

--- wp-content/themes/xevos-cyber-theme/template-parts/components/contact-info.php
+++ wp-content/themes/xevos-cyber-theme/template-parts/components/contact-info.php
@@ -24,7 +24,7 @@ if ($class) {
 	<?php if ($adresa) : ?>
 		<div class="xevos-contact-info__item">
 			<span class="xevos-contact-info__icon">
-				<img src="http://localhost/csc/wp-content/themes/xevos-cyber-theme/assets/img/global/pin-drop.svg" alt="" class="xevos-kontakt__icon">
+				<img src="<?= get_template_directory_uri(); ?>/assets/img/global/pin-drop.svg" alt="" class="xevos-kontakt__icon">
 			</span>
 			<a href="https://www.google.com/maps/place/XEVOS+Solutions/@49.8163154,18.2645651,19z/data=!3m1!4b1!4m6!3m5!1s0x4713e3435b1b7fef:0xdca3ef2a93b12439!8m2!3d49.8163145!4d18.2652088!16s%2Fg%2F1tghbwgx?entry=ttu&g_ep=EgoyMDI2MDMyNC4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener"><?php echo esc_html($adresa); ?></a>
 		</div>
@@ -32,7 +32,7 @@ if ($class) {
 	<?php if ($telefon) : ?>
 		<div class="xevos-contact-info__item">
 			<span class="xevos-contact-info__icon">
-				<img src="http://localhost/csc/wp-content/themes/xevos-cyber-theme/assets/img/global/phone.svg" alt="" class="xevos-kontakt__icon">
+				<img src="<?= get_template_directory_uri(); ?>/assets/img/global/phone.svg" alt="" class="xevos-kontakt__icon">
 			</span>
 			<a href="tel:<?php echo esc_attr(preg_replace('/\s+/', '', $telefon)); ?>"><?php echo esc_html($telefon); ?></a>
 		</div>
@@ -40,7 +40,7 @@ if ($class) {
 	<?php if ($email) : ?>
 		<div class="xevos-contact-info__item">
 			<span class="xevos-contact-info__icon">
-				<img src="http://localhost/csc/wp-content/themes/xevos-cyber-theme/assets/img/global/mail.svg" alt="" class="xevos-kontakt__icon">
+				<img src="<?= get_template_directory_uri(); ?>/assets/img/global/mail.svg" alt="" class="xevos-kontakt__icon">
 			</span>
 			<a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html($email); ?></a>
 		</div>
